3e87d4bbf43608a7bb8320aa79045bdeb62bd3b8
John <John@10.71.45.41>
2020-09-30 21:46:34 -0400

    This is the modified version

--- BankAccount/src/BankAccount.java
+++ BankAccount/src/BankAccount.java
@@ -2,42 +2,68 @@
    A bank account has a balance and a mechanism for
    applying interest or fees at the end of the month.
 */
-public class BankAccount 
-{
-   private double balance;
+public class BankAccount {
+	
+	
+	/**
+	   the current balance of the bank account
+	 */
+	private double balance;
+	
+	
+	
+	/**
+	   the interest percentage of the bank account
+	 */
+	final double interestPct = 0.05;
+	
+	
+	/**
+      Constructs a bank account with a given starting balance and interest percentage.
+      @param amount the starting balance of the bank account
+    */
+	public BankAccount(double amount) {
+		balance = amount;
+	}
 
-   /**
-      Constructs a bank account with zero balance.
-   */
-   public BankAccount(double amount)
-   {
-      balance = amount;
-   }
+   
+	/**
+      Makes a deposit into this bank account.
+      @param amount the amount of the deposit into the bank account
+    */
+	public void deposit(double amount) {
+		balance += amount;
+	}
 
+   
    /**
-      Makes a deposit into this account.
-      @param amount the amount of the deposit
-   */
-   public void deposit(double amount)
-   {
-      balance = balance + amount;
-   }
-
-   /**
-      Makes a withdrawal from this account, or charges a penalty if
+      Makes a withdrawal from this bank account, or displays a message if
       sufficient funds are not available.
-      @param amount the amount of the withdrawal
-   */
-   public void withdraw(double amount)
-   {
-      balance = balance - amount;
-   }
-   /**
+      @param amount the amount of the withdrawal from the bank account
+    */
+	public void withdraw(double amount) {
+		if (amount < balance) {
+		balance -= amount;
+		System.out.println(amount + " has been deducted from the account.");
+		} else {
+			System.out.println("Insufficient funds to support withdrawal.");
+		}
+	}
+   
+   
+	/**
       Gets the current balance of this bank account.
-      @return the current balance
-   */
-   public double getBalance()
-   {
-      return balance;
-   }
+	 */
+	public double getBalance() {
+		return balance;
+	}
+	
+	
+	/**
+	  Gets the interest percentage.
+	 */
+	public double getInterestPct() {
+		return interestPct;
+	
+	}
 }
--- BankAccount/src/BankAccountTester.java
+++ BankAccount/src/BankAccountTester.java
@@ -1,23 +1,38 @@
+import java.util.Scanner;
+
 public class BankAccountTester {
 	
 	public static void main(String[] args) {
-
-		//Create account myBankAccount with starting balance 1000
-		double startingBalance = 1000;
+		Scanner in = new Scanner(System.in);
+		
+		//Ask user for starting balance
+		System.out.print("Please enter the new account's starting balance: ");
+		double startingBalance = in.nextDouble();
+	
+		//Create account myBankAccount with starting balance and interest percentage
 		BankAccount myBankAccount = new BankAccount(startingBalance);
-		System.out.println("A new bank account called myBankAccount has been created with a starting balance of " + startingBalance + ".");
+		System.out.println("A new bank account has been created with a starting balance of " + startingBalance + " and an interest rate of " + myBankAccount.getInterestPct() + ".");
 		
-		//Withdraw 500 from myBankAccount
-		double withdrawAmount = 500;
-		myBankAccount.withdraw(withdrawAmount);
-		System.out.println(withdrawAmount + " has been deducted from the account.");
-		
-		//Deposit 1500 into myBankAccount
-		double depositAmount = 1500;
+		//Ask user for amount to deposit into myBankAccount and deposit that amount
+		System.out.print("\nPlease enter an amount to deposit into the account: ");
+		double depositAmount = in.nextDouble();
 		myBankAccount.deposit(depositAmount);
 		System.out.println(depositAmount + " has been deposited into the account.");
+				
+		//Ask user for amount to withdraw from myBankAccount and withdraw that amount
+		System.out.print("\nPlease enter an amount to withdraw from the account: ");
+		double withdrawAmount = in.nextDouble();
+		myBankAccount.withdraw(withdrawAmount);
 		
-		//Get and print the balance in myBankAccount
-		System.out.println("The current balance of this account is " + myBankAccount.getBalance() + ".");
+		//Calculate the interest as a product of the balance and interest rate.
+		double interest = myBankAccount.getBalance()*myBankAccount.getInterestPct();
+		
+		//Get and print the new balance in myBankAccount
+		System.out.println("\nThe new balance of this account is " + myBankAccount.getBalance() + ".");
+	
+		//Get and print the interest
+		System.out.println("The interest of this account is " + interest + ".");
+		
+		in.close();
 	}
 }
