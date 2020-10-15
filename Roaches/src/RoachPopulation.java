88c0662a32b269e4e55682a00e8f52c143ec2be8
John <John@LAPTOP-LBQS8J8C>
2020-10-14 21:03:46 -0400

    streamlined version using only one instance variable

--- Roaches/src/RoachPopulation.java
+++ Roaches/src/RoachPopulation.java
@@ -1,78 +1,40 @@
+
 public class RoachPopulation {
 	
 	/**
-		the number of Roaches in a population
+		Instance variable: the number of Roaches in a population
 	 */
 	private double roachPop;
-	
-	/**
-		the Roach population multiplier for breeding
-	 */
-	private double popMultiplier = 2;
-	
-	/**
-		the percentage of bug killer spray being used
-	 */
-	private double sprayPercent;
-	
-	/**
-		the Roach population after being sprayed with deadly bug killer
-	 */
-	private double roachPopAfterSpray;
-	
-	/**
-		keeps track of Roach breeding cycles
-	 */
-	private int breedCycle = 0;
 	
 	/**
 		Constructs a new Roach population
 		@param numRoaches accepts the number of Roaches in a population
 	 */
-	public void newRoaches(double numRoaches) {
+	public void construct(double numRoaches) {
 		roachPop = numRoaches;
-		System.out.println("The initial Roach population is " + (int)roachPop + ".");
 	}
 	
 	/**
-		simulates the Roach population breeding
+		Simulates breeding
 	 */
 	public void breed() {
-		//update value for Roach population after being sprayed
-		if(breedCycle > 0) {
-			roachPop = roachPopAfterSpray;
-		}
-		
-		//increment breed cycle
-		breedCycle ++;
-		
-		//increase Roach population by given breeding multiplier
-		roachPop *= popMultiplier;
+		//double Roach population
+		roachPop *= 2;
 	}
 	
 	/**
-		simulates spraying the Roach population with a deadly bug killer
-		@param sprayAmount accepts the strength of the bug killer spray as a percent 0-100
-		@return the accepted spray amount
+		Simulates spraying
+		@param sprayAmount accepts the strength of the spray as a percent 0-100
 	 */
-	public double spray(double sprayAmount) {
-		//pass argument to variable
-		sprayPercent = sprayAmount;
-		
-		//reduce Roach population by given percent and round up if necessary
-		roachPopAfterSpray = Math.ceil(roachPop*(1.0-sprayPercent/100.0));
-		
-		return sprayAmount;
+	public void spray(double sprayAmount) {
+		//reduce Roach population by a given percent from 0-100 and round up
+		roachPop = Math.ceil(roachPop*(1-(sprayAmount/100)));
 	}
 	
 	/**
-		gets and prints the number of Roaches after breeding and spraying
+		returns the number of Roaches in the population
 	 */
-	public void getRoaches() {
-		//print Roach population after breeding, without decimals for readability
-		System.out.println("Roach population after cycle " + breedCycle + " of breeding is " + (int)roachPop + ".");
-		
-		//print Roach population after breeding and then being sprayed by bug killer, without decimals for readability
-		System.out.println("Roach population after cycle " + breedCycle + " of breeding and spraying " + (int)sprayPercent + "% is " + (int)roachPopAfterSpray + ".");
+	public int getRoaches() {
+		return (int)roachPop;
 	}
 }
--- Roaches/src/RoachSimulation.java
+++ Roaches/src/RoachSimulation.java
@@ -1,3 +1,4 @@
+
 public class RoachSimulation {
 
 	public static void main(String[] args) {
@@ -5,30 +6,38 @@ public class RoachSimulation {
 		//create object called bugs
 		RoachPopulation bugs = new RoachPopulation();
 		
+		
 		//construct new Roach population with 10 Roaches
-		bugs.newRoaches(10);
+		bugs.construct(10);
+		System.out.println("The initial Roach population is " + bugs.getRoaches() + ".");
+		
 		
 		//breed Roaches to double population
 		bugs.breed();
+		System.out.println("The Roach population after cycle 1 breeding is " + bugs.getRoaches() + ".");
 		
-		//spray Roaches to reduce population by a given percentage
+		
+		//spray Roaches to reduce population by 50%
 		bugs.spray(50);
+		System.out.println("The Roach population after cycle 1 and 50% spraying is " + bugs.getRoaches() + ".");
 		
-		//print number of Roaches before and after spraying
-		bugs.getRoaches();
-		
-		//do another cycle with a larger percentage
+	
+		//do another cycle with 60% spray
 		bugs.breed();
+		System.out.println("The Roach population after cycle 2 breeding is " + bugs.getRoaches() + ".");
 		bugs.spray(60);
-		bugs.getRoaches();
+		System.out.println("The Roach population after cycle 2 and 60% spraying is " + bugs.getRoaches() + ".");
 		
-		//repeat two more times with even larger percentages
+		
+		//repeat two more times with 70% and 80% spray
 		bugs.breed();
+		System.out.println("The Roach population after cycle 3 breeding is " + bugs.getRoaches() + ".");
 		bugs.spray(70);
-		bugs.getRoaches();
+		System.out.println("The Roach population after cycle 3 and 70% spraying is " + bugs.getRoaches() + ".");
 		
 		bugs.breed();
+		System.out.println("The Roach population after cycle 4 breeding is " + bugs.getRoaches() + ".");
 		bugs.spray(80);
-		bugs.getRoaches();
+		System.out.println("The Roach population after cycle 4 and 80% spraying is " + bugs.getRoaches() + ".");
 	}
 }
